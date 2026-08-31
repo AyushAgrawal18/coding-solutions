7a917e5d6841695b28610cdc047d0bc674ffe2b9
AYUSH KUMAR AGRAWAL <167207663+AyushAgrawal18@users.noreply.github.com>
2026-08-31 19:40:42 +0530

    ✅ Wiggle Sort II | Medium | C++ | Runtime: 0 ms | Memory: 8.2 MB | 2026-08-31

--- leetcode/medium/0324-wiggle-sort-ii/solution.cpp
+++ leetcode/medium/0324-wiggle-sort-ii/solution.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     void wiggleSort(vector<int>& nums) {
         int n = nums.size();
-        vector<int> temp = nums;
-        sort(temp.begin(), temp.end());
+        // vector<int> temp = nums;
+        sort(nums.begin(), nums.end());
         int mid = (n - 1) / 2;
         int right = n - 1;
         for (int i = 0; i < n; i++) {
             if (i % 2 == 0) {
-                nums[i] = temp[mid--];
+                nums[i] = nums[mid--];
             } else {
-                nums[i] = temp[right--];
+                nums[i] = nums[right--];
             }
         }
     }
