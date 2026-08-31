a0e9be4169b78ec362dfac7f1dd38b009647a456
AYUSH KUMAR AGRAWAL <167207663+AyushAgrawal18@users.noreply.github.com>
2026-09-01 01:19:10 +0530

    ✅ Edit Distance | Medium | C++ | Runtime: 3 ms (91.51%) | Memory: 10.4 MB | 2026-08-31

--- leetcode/medium/0072-edit-distance/solution.cpp
+++ leetcode/medium/0072-edit-distance/solution.cpp
@@ -22,25 +22,29 @@ public:
         // vector<vector<int>> dp(n, vector<int> (m, -1));
         // return solve(n-1, m-1, a, b, dp);
 
-        vector<vector<int>> dp(n+1, vector<int> (m+1, 0));
+        // vector<vector<int>> dp(n+1, vector<int> (m+1, 0));
 
-        for(int i=0;i<=n;i++) dp[i][0]=i;
-        for(int i=0;i<=m;i++) dp[0][i]=i;
+        vector<int> prev(m+1, 0);
+        vector<int> curr(m+1, 0);
+
+        for(int i=0;i<=m;i++) prev[i]=i;
 
         for(int i=1;i<=n;i++){
+            curr[0]=i;
             for(int j=1;j<=m;j++){
 
                 if(a[i-1]==b[j-1]){
-                    dp[i][j]=dp[i-1][j-1];
+                    curr[j]=prev[j-1];
                 }
                 else{
-                    int insert = dp[i][j-1];
-                    int del = dp[i-1][j];
-                    int replace = dp[i-1][j-1];
-                    dp[i][j] = 1+min({insert, del, replace});
+                    int insert = curr[j-1];
+                    int del = prev[j];
+                    int replace = prev[j-1];
+                    curr[j] = 1+min({insert, del, replace});
                 }
             }
+            prev=curr;
         }
-        return dp[n][m];
+        return prev[m];
     }
 };
