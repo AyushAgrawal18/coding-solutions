59df05d7e89261a326096cd109b292673ff1b40a
AYUSH KUMAR AGRAWAL <167207663+AyushAgrawal18@users.noreply.github.com>
2026-09-01 01:16:49 +0530

    ✅ Edit Distance | Medium | C++ | Runtime: 3 ms (91.51%) | Memory: 13.3 MB | 2026-08-31

--- leetcode/medium/0072-edit-distance/solution.cpp
+++ leetcode/medium/0072-edit-distance/solution.cpp
@@ -19,7 +19,28 @@ public:
     int minDistance(string a, string b) {
         int n=a.size();
         int m=b.size();
-        vector<vector<int>> dp(n, vector<int> (m, -1));
-        return solve(n-1, m-1, a, b, dp);
+        // vector<vector<int>> dp(n, vector<int> (m, -1));
+        // return solve(n-1, m-1, a, b, dp);
+
+        vector<vector<int>> dp(n+1, vector<int> (m+1, 0));
+
+        for(int i=0;i<=n;i++) dp[i][0]=i;
+        for(int i=0;i<=m;i++) dp[0][i]=i;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+
+                if(a[i-1]==b[j-1]){
+                    dp[i][j]=dp[i-1][j-1];
+                }
+                else{
+                    int insert = dp[i][j-1];
+                    int del = dp[i-1][j];
+                    int replace = dp[i-1][j-1];
+                    dp[i][j] = 1+min({insert, del, replace});
+                }
+            }
+        }
+        return dp[n][m];
     }
 };
