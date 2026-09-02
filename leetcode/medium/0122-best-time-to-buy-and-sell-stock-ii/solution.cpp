f35dcc8896e95381f645c3030a6b0f3934345baa
AYUSH KUMAR AGRAWAL <167207663+AyushAgrawal18@users.noreply.github.com>
2026-09-02 23:07:32 +0530

    ✅ Best Time to Buy and Sell Stock II | Medium | C++ | Runtime: 10 ms (32.64%) | Memory: 24.9 MB | 2026-09-02

--- leetcode/medium/0122-best-time-to-buy-and-sell-stock-ii/solution.cpp
+++ leetcode/medium/0122-best-time-to-buy-and-sell-stock-ii/solution.cpp
@@ -19,7 +19,22 @@ public:
 
     int maxProfit(vector<int>& prices) {
         int buy = 1;
-        vector<vector<int>> dp(prices.size()+1, vector<int> (2, -1));
-        return solve(1, prices, buy, dp);
+        int n=prices.size();
+        // vector<vector<int>> dp(prices.size()+1, vector<int> (2, -1));
+        // return solve(1, prices, buy, dp);
+        vector<vector<int>> dp(n+2, vector<int>(2,0));
+        for (int i = n; i >= 1; i--) {
+
+            dp[i][1] = max(
+                -prices[i - 1] + dp[i + 1][0],  
+                dp[i + 1][1]                    
+            );
+
+            dp[i][0] = max(
+                prices[i - 1] + dp[i + 1][1],   
+                dp[i + 1][0]                    
+            );
+        }
+        return dp[1][1];
     }
 };
